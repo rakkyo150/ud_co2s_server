60d34ce147cc043f6c53ab4232fbe936e0cd9f31
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2023-08-13 02:33:53 +0900

    とりあえず完成

---
+++ src/main.rs
@@ -0,0 +1,176 @@
+use actix_web::{get, App, HttpServer, Responder};
+use serial::SerialPort;
+use std::io::{self, prelude::*};
+use std::time::SystemTime;
+use serde::{Serialize,Deserialize};
+use std::env;
+
+const DEVICE_PATH: &str = "/dev/ttyACM0";
+
+#[derive(Serialize,Deserialize,Debug)]
+struct UDCO2SStat {
+    co2ppm: i32,
+    humidity: f32,
+    temperature: f32,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+pub struct Log{
+    time: i64,
+    status: UDCO2SStat
+}
+
+impl UDCO2SStat {
+    fn new(co2ppm: i32, humidity: f32, temperature: f32) -> Self {
+        UDCO2SStat {
+            co2ppm,
+            humidity,
+            temperature,
+        }
+    }
+}
+
+pub struct UDCO2S {
+    dev: String,
+}
+
+#[get("/all")]
+async fn all() -> impl Responder {
+    let sensor = UDCO2S::new(DEVICE_PATH);
+    let log = sensor.start_logging();
+    match log{
+        Ok(log) => {
+            format!("{:?}", log)
+        },
+        Err(e) => {
+            format!("{}", e)
+        }
+    }
+}
+
+#[get("/co2")]
+async fn co2() -> impl Responder {
+    let sensor = UDCO2S::new(DEVICE_PATH);
+    let log = sensor.start_logging();
+    match log{
+        Ok(log) => {
+            format!("{}", log.status.co2ppm)
+        },
+        Err(e) => {
+            format!("{}", e)
+        }
+    }
+}
+
+#[get("/hum")]
+async fn hum() -> impl Responder {
+    let sensor = UDCO2S::new(DEVICE_PATH);
+    let log = sensor.start_logging();
+    match log{
+        Ok(log) => {
+            format!("{}", log.status.humidity)
+        },
+        Err(e) => {
+            format!("{}", e)
+        }
+    }
+}
+
+#[get("/tmp")]
+async fn tmp() -> impl Responder {
+    let sensor = UDCO2S::new(DEVICE_PATH);
+    let log = sensor.start_logging();
+    match log{
+        Ok(log) => {
+            format!("{}", log.status.temperature)
+        },
+        Err(e) => {
+            format!("{}", e)
+        }
+    }
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    HttpServer::new(|| App::new().service(all).service(co2).service(hum).service(tmp))
+        .bind((env::var("LOCAL_ADDRESS").unwrap(), 5980))?
+        .run()
+        .await
+}
+
+impl UDCO2S {
+    pub fn new(dev: &str) -> Self {
+        UDCO2S {
+            dev: dev.into(),
+        }
+    }
+    
+    pub fn start_logging(&self) -> io::Result<Log> {
+        let regex = regex::Regex::new(r"CO2=(?P<co2>\d+),HUM=(?P<hum>\d+\.\d+),TMP=(?P<tmp>-?\d+\.\d+)").unwrap();
+        
+        let mut port = serial::open(&self.dev).unwrap();
+
+        let option_func = &|settings: &mut dyn serial::SerialPortSettings|{
+            _ = settings.set_baud_rate(serial::Baud115200);
+            settings.set_char_size(serial::Bits8);
+            settings.set_parity(serial::ParityNone);
+            settings.set_stop_bits(serial::Stop1);
+            settings.set_flow_control(serial::FlowNone);
+            Ok(())
+        };
+
+        _ = port.reconfigure(option_func);
+        _ = port.set_timeout(std::time::Duration::from_secs(6));
+        
+        write!(&mut port, "STA\r\n")?;
+        print!("{}", read_until(&mut port, '\n')?); // Print the first line
+        
+        if let Ok(line) = read_until(&mut port, '\n') {
+            if let Some(m) = regex.captures(&line) {
+                let time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as i64;
+
+                let obj=Log{
+                    time : time_now,
+                    status:UDCO2SStat::new(
+                        m["co2"].parse::<i32>().unwrap(),
+                        m["hum"].parse::<f32>().unwrap(),
+                        m["tmp"].parse::<f32>().unwrap()
+                )};
+
+                write!(&mut port, "STP\r\n")?;
+                return Ok(obj);
+                // return Ok(m["co2"].parse::<i32>().unwrap().to_string());
+            }
+        }
+        
+        write!(&mut port, "STP\r\n")?;
+
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Could not read from device",
+        ))
+    }
+}
+
+fn read_until(port: &mut dyn serial::SerialPort, del: char) -> io::Result<String> {
+    let mut res = String::new();
+    loop {
+        let mut buf = [0u8; 1];
+        match port.read_exact(&mut buf) {
+            Ok(_) => {
+                let ch = char::from(buf[0]);
+                if ch == del {
+                    return Ok(res);
+                } else {
+                    res.push(ch);
+                }
+            }
+            Err(e) => match e.kind() {
+                io::ErrorKind::TimedOut => return Ok(res),
+                _ => return Err(e.into()),
+            },
+        }
+    }
+}
